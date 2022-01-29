3fd8faabbcef6ef19bacf75827d330f0a3fcd376
Lastril <76162135+Lastril@users.noreply.github.com>
2022-01-29 19:03:51 +0100

    SV Kit

--- UHC/src/fr/lastril/uhchost/modes/lg/roles/village/Villageois.java
+++ UHC/src/fr/lastril/uhchost/modes/lg/roles/village/Villageois.java
@@ -1,17 +1,24 @@
 package fr.lastril.uhchost.modes.lg.roles.village;
 
+import fr.lastril.uhchost.UhcHost;
+import fr.lastril.uhchost.enums.Messages;
 import fr.lastril.uhchost.modes.lg.roles.LGRole;
 import fr.lastril.uhchost.modes.roles.Camps;
 import fr.lastril.uhchost.modes.roles.Role;
+import fr.lastril.uhchost.tools.API.items.Livre;
 import fr.lastril.uhchost.tools.API.items.crafter.QuickItem;
 import org.bukkit.Material;
 import org.bukkit.SkullType;
+import org.bukkit.enchantments.Enchantment;
 import org.bukkit.entity.Player;
 
 public class Villageois extends Role implements LGRole {
 
+    private final int randomKit = UhcHost.getRANDOM().nextInt(6);
+
     @Override
     public void giveItems(Player player) {
+        giveKit(player);
     }
 
     @Override
@@ -54,6 +61,38 @@ public class Villageois extends Role implements LGRole {
     @Override
     public Camps getCamp() {
         return Camps.VILLAGEOIS;
+    }
+
+    public void giveKit(Player player){
+        switch (randomKit){
+            case 0:
+                player.sendMessage(Messages.LOUP_GAROU_PREFIX.getMessage() + "§cVoici votre kit: Mineur");
+                main.getInventoryUtils().giveItemSafely(player,new QuickItem(Material.DIAMOND_PICKAXE).addEnchant(Enchantment.DIG_SPEED, 2, true).toItemStack());
+                break;
+            case 1:
+                player.sendMessage(Messages.LOUP_GAROU_PREFIX.getMessage() + "§cVoici votre kit: Forgeron");
+                main.getInventoryUtils().giveItemSafely(player, new QuickItem(Material.ANVIL).toItemStack());
+                main.getInventoryUtils().giveItemSafely(player, new QuickItem(Material.EXP_BOTTLE, 20).toItemStack());
+                break;
+            case 2:
+                player.sendMessage(Messages.LOUP_GAROU_PREFIX.getMessage() + "§cVoici votre kit: Archer");
+                main.getInventoryUtils().giveItemSafely(player, new QuickItem(Material.ARROW, 64).toItemStack());
+                main.getInventoryUtils().giveItemSafely(player, new QuickItem(Material.STRING, 6).toItemStack());
+                break;
+            case 3:
+                player.sendMessage(Messages.LOUP_GAROU_PREFIX.getMessage() + "§cVoici votre kit: Meurtrier");
+                main.getInventoryUtils().giveItemSafely(player, new Livre(Enchantment.DAMAGE_ALL, 3).toItemStack());
+                break;
+            case 4:
+                player.sendMessage(Messages.LOUP_GAROU_PREFIX.getMessage() + "§cVoici votre kit: Libraire");
+                main.getInventoryUtils().giveItemSafely(player, new QuickItem(Material.BOOK, 8 ).toItemStack());
+                main.getInventoryUtils().giveItemSafely(player, new QuickItem(Material.EXP_BOTTLE, 20).toItemStack());
+                break;
+            case 5:
+                player.sendMessage(Messages.LOUP_GAROU_PREFIX.getMessage() + "§cVoici votre kit: Armurier");
+                main.getInventoryUtils().giveItemSafely(player, new QuickItem(Material.DIAMOND_BOOTS).addEnchant(Enchantment.PROTECTION_ENVIRONMENTAL, 1, true).toItemStack());
+                break;
+        }
     }
 
 }
